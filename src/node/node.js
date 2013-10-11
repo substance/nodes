8542e67a8f4e21a1195a48518bb0e4eb8c0075ca
michael <michael@substance.io>
2013-10-11 11:24:49 +0200

    Docs.

--- src/node/node.js
+++ src/node/node.js
@@ -2,4 +2,26 @@
 
 // Note: we leave the Node in `substance-document` as it is an essential part of the API.
 var Document = require("substance-document");
-module.exports = Document.Node;
+
+
+var Node = Document.Node;
+
+// This is used for the auto-generated docs
+// -----------------
+//
+
+Node.description = {
+  "name": "Node",
+  "remarks": [
+    "Abstract node type."
+  ],
+  "properties": {
+    "source_id": "Useful for document conversion where the original id of an element should be remembered.",
+  }
+};
+
+// Example
+// -------
+//
+
+module.exports = Node;
