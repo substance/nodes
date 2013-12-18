6cf83f067cacf7a5202772155887df8209a737c4
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-12-18 16:38:20 +0100

    Basic implementation of view components.
    ViewComponents are used to describe the structure of a rendered view
    and sets it into relation with the model.
    
    E.g., a conventional component is a NodeComponent. This tells that
    a view component is a full fledged, top-level node view.
    On the other side, a PropertyComponent is responsible for a node's property.

---
+++ src/view_components.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var __createRange__ = function(el) {
+  var range = document.createRange();
+  range.selectNode(el);
+  return range;
+};
+
+var AbstractComponent = function(node, view, el) {
+  this.node = node;
+  this.view = view;
+  this.el = el;
+  this.__range__ = null;
+};
+AbstractComponent.Prototype = function() {
+  this.getRange = function() {
+    if (!this.__range__) {
+      this.__range__ = __createRange__(this.el);
+    }
+    return this.__range__;
+  };
+};
+AbstractComponent.prototype = new AbstractComponent.Prototype();
+
+var NodeComponent = function(node, view) {
+  AbstractComponent.call(this, node, view, view.el);
+  this.type = "node";
+  this.path = [node.id];
+};
+NodeComponent.Prototype = function() {
+  this.getLength = function() {
+    return this.node.getLength();
+  };
+};
+NodeComponent.prototype.prototype = AbstractComponent.prototype;
+NodeComponent.prototype = new NodeComponent.Prototype();
+
+var PropertyComponent = function(node, view, property, el) {
+  AbstractComponent.call(this, node, view, el);
+  this.type = "property";
+  this.path = [node.id, property];
+};
+PropertyComponent.Prototype = function() {
+  this.getLength = function() {
+    throw new Error("This is abstract and must be overridden");
+  };
+};
+PropertyComponent.prototype.prototype = AbstractComponent.prototype;
+PropertyComponent.prototype = new PropertyComponent.Prototype();
+
+module.exports = {
+  NodeComponent: NodeComponent,
+  PropertyComponent: PropertyComponent
+};
