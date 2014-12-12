05d1f23f0bebf761ebcf8599286fa93c2aa6472a
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-12-12 23:42:52 +0100

    Create fragments automatically when creating multi-annotations.

--- src/multi_annotation/multi_annotation.js
+++ src/multi_annotation/multi_annotation.js
@@ -1,10 +1,11 @@
 "use strict";
 
 var DocumentNode = require('../node/node');
-var _ = require('underscore');
+var util = require('substance-util');
 
 var MultiAnnotation = function(node, document) {
   DocumentNode.call(this, node, document);
+  this.addFragments();
 };
 
 // Type definition
@@ -13,14 +14,50 @@ var MultiAnnotation = function(node, document) {
 MultiAnnotation.type = {
   "id": "annotation",
   "properties": {
+    "container": "string",
     "startPath": ["array", "string"],
-    "startPos": "number",
+    "startCharPos": "number",
     "endPath": ["array", "string"],
-    "endPos": "number"
+    "endCharPos": "number"
   }
 };
 
 MultiAnnotation.Prototype = function() {
+
+  this.addFragments = function() {
+    var container = this.document.get(this.container);
+    var annotationId = this.id;
+    var startComp = container.lookup(this.startPath);
+    var endComp = container.lookup(this.endPath);
+    var start = startComp.pos;
+    var end = endComp.pos;
+    debugger;
+    for (var i = start; i <= end; i++) {
+      var comp, startCharPos, endCharPos;
+      if (i === start) {
+        comp = startComp;
+        startCharPos = this.startCharPos;
+        endCharPos = comp.getLength();
+      } else if (i === end) {
+        comp = endComp;
+        startCharPos = 0;
+        endCharPos = this.endCharPos;
+      } else {
+        comp = container.getComponent(i);
+        startCharPos = 0;
+        endCharPos = comp.getLength();
+      }
+      var annotationFragment = {
+        "type": "annotation_fragment",
+        "id": util.uuid('fragment_'),
+        "annotation_id": annotationId,
+        "path": comp.path,
+        "range": [startCharPos, endCharPos]
+      };
+      this.document.create(annotationFragment);
+    }
+  };
+
 };
 
 MultiAnnotation.Prototype.prototype = DocumentNode.prototype;
